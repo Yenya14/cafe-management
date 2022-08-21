83fbb8dfdb9186337c6ae9f447aa87a1a910d15d
Yenya14 <leeza.mhr@gmail.com>
2022-08-22 00:01:44 +0545

    Comiccafe Commit

---
+++ index.php
@@ -0,0 +1,352 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Comic Cafe</title>
+
+    <link rel="stylesheet" href="https://unpkg.com/swiper@7/swiper-bundle.min.css" />
+
+    <!-- font awesome cdn link  -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.15.4/css/all.min.css">
+
+    <!-- custom css file link  -->
+    <link rel="stylesheet" href="css/style.css">
+
+</head>
+<body>
+    
+<!-- header section starts  -->
+
+<header class="header">
+
+    <div id="menu-btn" class="fas fa-bars"></div>
+
+    <a href="#" class="logo"> Comic Cafe<i class="fas fa-mug-hot"></i> </a>
+
+    <nav class="navbar">
+        <a href="#home">home</a>
+        <a href="#about">about</a>
+        <a href="#menu">menu</a>
+        <a href="#order">order</a>
+    </nav>
+
+    <a href="#" class="btn">BEST-TEAS</a>
+
+</header>
+
+<section class="home" id="home">
+
+    <div class="row">
+
+        <div class="content">
+            <h3>Free sticker with every drinks</h3>
+            <a href="#order" class="btn">buy one now</a>
+        </div>
+
+        <div class="image">
+            <img src="image/home-img-1.png" class="main-home-image" alt="">
+        </div>
+
+    </div>
+
+    <div class="image-slider">
+        <img src="image/home-img-1.png" alt="">
+        <img src="image/home-img-2.png" alt="">
+        <img src="image/home-img-3.png" alt="">
+    </div>
+
+</section>
+
+<section class="about" id="about">
+
+    <h1 class="heading"> about us <span>why choose us</span> </h1>    
+
+    <div class="row">
+
+        <div class="image">
+            <img src="image/about-img.png" alt="">
+        </div>
+
+        <div class="content">
+            <h3 class="title">what's make our boba special!</h3>
+            <p>The sweet taste of the tea paired with the chewy tapioca balls make up the unique element that boba provides. Drinks like iced coffee or juice have the same constant texture, but with boba, the pearls offer a sort of break from the same boring consistency. There really is no other drink like it.</p>
+            
+            <div class="icons-container">
+                <div class="icons">
+                    <img src="image/about-icon-1.png" alt="">
+                    <h3>quality drinks and deserts</h3>
+                </div>
+                <div class="icons">
+                    <img src="image/about-icon-2.png" alt="">
+                    <h3>2D-Themed</h3>
+                </div>
+                <div class="icons">
+                    <img src="image/about-icon-3.png" alt="">
+                    <h3>delivery options</h3>
+                </div>
+            </div>
+        </div>
+
+    </div>
+
+</section>
+
+<section class="menu" id="menu">
+
+    <h1 class="heading"> our menu <span>categories</span> </h1>
+
+    <div class="box-container">
+
+        <a href="#hotdrinks" class="box">
+            <img src="image/menu-6.png" alt="">
+            <div class="content">
+                <h3>Hot Drinks</h3>
+            </div>
+        </a>
+
+        <a href="#colddrinks" class="box">
+            <img src="image/menu-1.png" alt="">
+            <div class="content">
+                <h3>Cold Drinks</h3>
+            </div>
+        </a>
+
+        <a href="#bubbleteas" class="box">
+            <img src="image/menu-3.jpg" alt="">
+            <div class="content">
+                <h3>Bubble Teas</h3>
+            </div>
+        </a>
+
+        <a href="#donuts" class="box">
+            <img src="image/donut-1.png" alt="">
+            <div class="content">
+                <h3>Donuts</h3>
+            </div>
+        </a>
+
+
+</section>
+
+<section class="menu" id="hotdrinks">
+
+    <h1 class="heading"> our menu <span>Hot Drinks</span> </h1>
+
+    <div class="box-container">
+
+        <a href="#order" class="box">
+            <img src="image/hot-1.png" alt="">
+            <div class="content">
+                <h3>Espresso</h3>
+                <p>Espresso is a stronger black coffee. It's made from the same beans as coffee but is stronger, thicker, and higher in caffeine.</p>
+                <span>Rs.100</span>
+            </div>
+        </a>
+
+        <a href="#order" class="box">
+            <img src="image/hot-1.png" alt="">
+            <div class="content">
+                <h3>Capuccino</h3>
+                <p>Cappuccino is an espresso-based coffee drink and is prepared with steamed milk foam.</p>
+                <span>Rs.165</span>
+            </div>
+        </a>
+
+        <a href="#order" class="box">
+            <img src="image/hot-1.png" alt="">
+            <div class="content">
+                <h3>Cafe Mocha</h3>
+                <p>It is a chocolate-flavoured warm beverage that is a variant of a caffè latte.</p>
+                <span>Rs.195</span>
+            </div>
+        </a>
+
+        <a href="#order" class="box">
+            <img src="image/hot-1.png" alt="">
+            <div class="content">
+                <h3>Cafe Latte</h3>
+                <p>Latte is a coffee beverage made with espresso and steamed milk. </p>
+                <span>Rs.185</span>
+            </div>
+        </a>
+
+    </div>
+
+</section>
+
+<section class="menu" id="colddrinks">
+
+    <h1 class="heading"> our menu <span>Cold Drinks</span> </h1>
+
+    <div class="box-container">
+
+        <a href="#order" class="box">
+            <img src="image/menu-1.png" alt="">
+            <div class="content">
+                <h3>Iced Peach Tea</h3>
+                <p>Perfect summer drink which is good for health too.</p>
+                <span>Rs.170</span>
+            </div>
+        </a>
+
+        <a href="#order" class="box">
+            <img src="image/menu-2.png" alt="">
+            <div class="content">
+                <h3>Iced Americano</h3>
+                <p>It is a refreshing, bold and bitter drink.</p>
+                <span>Rs.155</span>
+            </div>
+        </a>
+
+        <a href="#order" class="box">
+            <img src="image/menu-3.jpg" alt="">
+            <div class="content">
+                <h3>Iced Latte</h3>
+                <p>It is a drink with espresso and frothed milk.</p>
+                <span>Rs.170</span>
+            </div>
+        </a>
+
+        <a href="#order" class="box">
+            <img src="image/menu-4.png" alt="">
+            <div class="content">
+                <h3>Flavoured Latte</h3>
+                <p>Optional caramel. vanilla, hazelnut flavour </p>
+                <span>Rs.235</span>
+            </div>
+        </a>
+
+    </div>
+
+</section>
+
+<section class="menu" id="bubbleteas">
+
+    <h1 class="heading"> our menu <span>Bubble Teas</span> </h1>
+
+    <div class="box-container">
+
+        <a href="#order" class="box">
+            <img src="image/menu-1.png" alt="">
+            <div class="content">
+                <h3>Strawberry</h3>
+                <p>Sweet and fruity like strawberries, with a cool, creamy texture from the milk and fun bursts of chewy boba in every sip.</p>
+                <span>Rs.250</span>
+            </div>
+        </a>
+
+        <a href="#order" class="box">
+            <img src="image/menu-2.png" alt="">
+            <div class="content">
+                <h3>Blueberry</h3>
+                <p>Made of blueberry milk, tapioca pearls, brewed tea, and served with loads of ice.</p>
+                <span>Rs.250</span>
+            </div>
+        </a>
+
+        <a href="#order" class="box">
+            <img src="image/menu-3.jpg" alt="">
+            <div class="content">
+                <h3>Taro</h3>
+                <p>It has a very sweet taste, with a slight hint of vanilla.</p>
+                <span>Rs.250</span>
+            </div>
+        </a>
+
+        <a href="#order" class="box">
+            <img src="image/menu-4.png" alt="">
+            <div class="content">
+                <h3>Chocolate</h3>
+                <p>It tastes a bit like ice-cold hot chocolate, but with an extra surprise tapioca pearls at the bottom.</p>
+                <span>Rs.250</span>
+            </div>
+        </a>
+
+    </div>
+
+</section>
+
+<section class="menu" id="donuts">
+
+    <h1 class="heading"> our menu <span>donuts</span> </h1>
+
+    <div class="box-container">
+
+        <a href="#order" class="box">
+            <img src="image/donut-1.png" alt="">
+            <div class="content">
+                <h3>Single Shot Triple Chococlate Donut</h3>
+                <span>Rs.215</span>
+            </div>
+        </a>
+
+        <a href="#order" class="box">
+            <img src="image/donut-2.png" alt="">
+            <div class="content">
+                <h3>Double Shot Double Chocolate Donut</h3>
+                <span>Rs.235</span>
+            </div>
+        </a>
+    </div>
+
+</section>
+
+<section class="book" id="order">
+
+    <h1 class="heading"> Try It <span>Order Now</span> </h1>
+
+    <form action="">
+
+        <input type="text" placeholder="your name" class="box">
+        <input type="email" placeholder="your email" class="box">
+        <input type="text" placeholder="your number" class="box">
+        <input type="text" placeholder="your address" class="box">
+
+        <textarea name="" placeholder="your message" class="box" id="" cols="30" rows="10"></textarea>
+
+        <input type="submit" value="send message" class="btn">
+
+    </form>
+
+</section>
+
+<section class="footer">
+
+    <div class="box-container">
+
+        <div class="box">
+            <h3>quick links</h3>
+            <a href="#home"> <i class="fas fa-arrow-right"></i> home </a>
+            <a href="#about"> <i class="fas fa-arrow-right"></i> about </a>
+            <a href="#menu"> <i class="fas fa-arrow-right"></i> menu </a>
+            <a href="#order"> <i class="fas fa-arrow-right"></i> order </a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="#"> <i class="fas fa-phone"></i> +977-980-1112255 </a>
+            <a href="#"> <i class="fas fa-envelope"></i> thecomiccafe.np@gmail.com </a>
+            <a href="#"> <i class="fas fa-envelope"></i> Basantapur, Kathmandu, Nepal </a>
+        </div>
+
+        <div class="box">
+            <h3>contact info</h3>
+            <a href="https://m.facebook.com/Thecomiccafenp-100797172147518/"> <i class="fab fa-facebook-f"></i> facebook </a>
+            <a href="https://www.instagram.com/thecomiccafe.np/?hl=en"> <i class="fab fa-instagram"></i> instagram </a>
+            <a href="https://www.linkedin.com/in/rohit-shrestha-6b9819178/?originalSubdomain=np"> <i class="fab fa-linkedin"></i> linkedin </a>
+        </div>
+
+    </div>
+
+    <div class="credit"> created by Liza Maharjan | all rights reserved </div>
+
+</section>
+
+<script src="https://unpkg.com/swiper@7/swiper-bundle.min.js"></script>
+
+<script src="js/script.js"></script>
+
+</body>
+</html>
